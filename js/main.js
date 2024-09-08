5891002ff4500c6d153281eee53f3eab19997abd
Aashay Gupte <151937841+conterating@users.noreply.github.com>
2024-09-07 21:24:01 -0400

    Dialog modal is now cleared on each opening

--- js/main.js
+++ js/main.js
@@ -6,97 +6,102 @@ function Book(title, author, pages, readStatus) {
   this.author = author;
   this.pages = pages;
   this.readStatus = readStatus;
-  this.info = function () {
-    return `${this.title} by ${this.author}, ${this.pages}, ${this.readStatus}`;
-  };
-  this.display = function () {
-    const card = document.createElement("div");
-    card.classList.add("card");
-    const libraryContainer = document.querySelector(".library-container");
-
-    //title
-    const title = document.createElement("h2");
-    title.textContent = `${this.title}`;
-    title.classList.add("bookTitle");
-    //author
-    const author = document.createElement("p");
-    author.textContent = `Author: ${this.author}`;
-    //pages
-    const pages = document.createElement("p");
-    pages.textContent = `Pages: ${this.pages}`;
-    //readStatus
-    const readStatus = document.createElement("p");
-    readStatus.textContent = `Status: ${this.readStatus}`;
-
-    card.append(title, author, pages, readStatus);
-    libraryContainer.append(card);
-  };
 }
 
-function addBookToLibrary() {
-  //do stuff here
-  const harryPotter = new Book("Harry Potter", "J. K. Rowling", 652, "read");
+Book.prototype.info = function () {
+  return `${this.title} by ${this.author}, ${this.pages}, ${this.readStatus}`;
+};
 
-  const percyJackson = new Book(
-    "Percy Jackson",
-    "Rick Riordan",
-    377,
-    "not read"
+Book.prototype.display = function () {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  const libraryContainer = document.querySelector(".library-container");
+
+  //title
+  const title = document.createElement("h2");
+  title.textContent = `${this.title}`;
+  title.classList.add("bookTitle");
+  //author
+  const author = document.createElement("p");
+  author.textContent = `Author: ${this.author}`;
+  //pages
+  const pages = document.createElement("p");
+  pages.textContent = `Pages: ${this.pages}`;
+  //readStatus
+  const readStatus = document.createElement("p");
+  readStatus.textContent = `Status: ${this.readStatus}`;
+
+  card.append(title, author, pages, readStatus);
+  libraryContainer.append(card);
+};
+
+const showButton = document.querySelector("#showDialog");
+const newFormModal = document.querySelector("#addNewBookForm");
+const confirmButton = document.querySelector("#confirmBtn");
+
+const harryPotter = new Book("Harry Potter", "J. K. Rowling", 652, "read");
+
+const percyJackson = new Book("Percy Jackson", "Rick Riordan", 377, "not read");
+
+const greatGatsby = new Book(
+  "Great Gatsby",
+  "F. Scott Fitzgerald",
+  180,
+  "read"
+);
+const keeperOfLostCities = new Book(
+  "Keeper of Lost Cities",
+  "Shannon Messenger",
+  512,
+  "read"
+);
+const diaryOfAWimpyKid = new Book(
+  "Diary of a Wimpy Kid",
+  "Jeff Kinney",
+  217,
+  "read"
+);
+const dune = new Book("Dune", "Frank Herbert", 412, "not read");
+//add preset books to myLibrary arrary
+myLibrary.push(
+  harryPotter,
+  percyJackson,
+  greatGatsby,
+  keeperOfLostCities,
+  diaryOfAWimpyKid,
+  dune
+);
+
+showButton.addEventListener("click", () => {
+  newFormModal.showModal();
+});
+
+newFormModal.addEventListener("close", (e) => {
+  const bookTitle = document.querySelector("#title");
+  const bookAuthor = document.querySelector("#author");
+  const bookPages = document.querySelector("#pages");
+  const bookReadTracker = document.querySelector("#readTracker");
+
+  console.log(
+    bookTitle.value,
+    bookAuthor.value,
+    bookPages.value,
+    bookReadTracker.value
   );
 
-  const greatGatsby = new Book(
-    "Great Gatsby",
-    "F. Scott Fitzgerald",
-    180,
-    "read"
-  );
-  const keeperOfLostCities = new Book(
-    "Keeper of Lost Cities",
-    "Shannon Messenger",
-    512,
-    "read"
-  );
-  const diaryOfAWimpyKid = new Book(
-    "Diary of a Wimpy Kid",
-    "Jeff Kinney",
-    217,
-    "read"
-  );
-  const dune = new Book("Dune", "Frank Herbert", 412, "not read");
-  //add preset books to myLibrary arrary
-  myLibrary.push(
-    harryPotter,
-    percyJackson,
-    greatGatsby,
-    keeperOfLostCities,
-    diaryOfAWimpyKid,
-    dune
-  );
+  bookTitle.value = "";
+  bookAuthor.value = "";
+  bookPages.value = "";
+  bookReadTracker.value = "";
+});
 
-  let wantstoAddBook = prompt("Do you want to add a book? ").toLowerCase();
+confirmButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  newFormModal.close();
+});
 
-  while (wantstoAddBook === "yes") {
-    let userBook = new Book(
-      prompt("What is the title? "),
-      prompt("Who is the author? "),
-      prompt("How many pages? "),
-      prompt("Read or Not Read? ").toLowerCase()
-    );
-
-    //append userBook object to Library
-    myLibrary.push(userBook);
-
-    let wantsAnotherBook = prompt(
-      "Do you want to add another book?"
-    ).toLowerCase();
-
-    //add another book
-    if (wantsAnotherBook == "yes") {
-      continue;
-    } else {
-      wantstoAddBook = false;
-    }
-  }
+function clearForm() {
+  document.getElementById("myForm").reset();
 }
 
 function displayBook() {
@@ -105,5 +110,4 @@ function displayBook() {
   }
 }
 
-addBookToLibrary();
 displayBook();
